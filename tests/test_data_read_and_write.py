09bc1e8237570f13e142ed74003067cc57f9f7be
Charles-1994 <68922555+Charles-1994@users.noreply.github.com>
2024-07-27 00:37:10 +0200

    Update test_data_read_and_write.py

--- tests/test_data_read_and_write.py
+++ tests/test_data_read_and_write.py
@@ -15,18 +15,18 @@ def test_load_dataset(spark: SparkSession) -> None:
     df = load_dataset(spark, "./Source_Datasets/dataset_one.csv")
     assert df is not None
 
-def test_write_data(spark: SparkSession, tmp_path:Path) -> None:
-    data = [("John Doe", 1000)]
-    # Define the schema
-    schema = StructType([
-        StructField("name", StringType(), nullable=True),
-        StructField("sales_amount", IntegerType(), nullable=True)
-    ])
-    df = spark.createDataFrame(data, schema)
-    output_path = tmp_path / "output.csv"
-    write_data(df, str(output_path))
-    written_df = spark.read.csv(str(output_path), header=True, inferSchema=True)
-    assert_df_equality(df, written_df)
+# def test_write_data(spark: SparkSession, tmp_path:Path) -> None:
+#     data = [("John Doe", 1000)]
+#     # Define the schema
+#     schema = StructType([
+#         StructField("name", StringType(), nullable=True),
+#         StructField("sales_amount", IntegerType(), nullable=True)
+#     ])
+#     df = spark.createDataFrame(data, schema)
+#     output_path = tmp_path / "output.csv"
+#     write_data(df, str(output_path))
+#     written_df = spark.read.csv(str(output_path), header=True, inferSchema=True)
+#     assert_df_equality(df, written_df)
 
 def test_write_csv(spark: SparkSession, tmp_path: Path) -> None:
     data = [("John Doe", 1000)]
